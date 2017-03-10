6524d9575443b1507076993281e68ea5252212af
milady495a <mtornerorubio@gmail.com>
2017-03-10 21:34:52 +0100

    outputs q faltaban

--- database/seeds/TiposTableSeeder.php
+++ database/seeds/TiposTableSeeder.php
@@ -13,10 +13,11 @@ class TiposTableSeeder extends Seeder
     {
       DB::table('tipos')->insert([
         ['id' => 1, 'name' => 'Personajes', 'name_trans' => 'personajes', 'icon' => 'user'],
-        ['id' => 2, 'name' => 'Escenarios', 'name_trans' => 'escenarios', 'icon' => 'globe'],
+        ['id' => 2, 'name' => 'Escenarios', 'name_trans' => 'escenarios', 'icon' => 'map-marker'],
         ['id' => 3, 'name' => 'Objetos', 'name_trans' => 'objetos', 'icon' => 'key'],
         ['id' => 4, 'name' => 'Situaciones', 'name_trans' => 'situaciones', 'icon' => 'puzzle-piece'],
         ['id' => 5, 'name' => 'Temas', 'name_trans' => 'temas', 'icon' => 'comments-o']
+        ['id' => 6, 'name' => 'Regiones', 'name_trans' => 'regiones', 'icon' => 'globe']
       ]);
     }
 }
